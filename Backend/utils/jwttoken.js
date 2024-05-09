a6ab318ce0f2e3cbb5a0892ccb6d02f23d1da1e8
Abhishek kumar <88671726+tyrantstark@users.noreply.github.com>
2024-05-09 21:55:32 +0530

    Update jwttoken.js

--- Backend/utils/jwttoken.js
+++ Backend/utils/jwttoken.js
@@ -8,7 +8,7 @@ export const sendToken = (user, statusCode, res, message) => {
      
       httpOnly: true, 
       secure:true,
-      sameSite:None,
+      sameSite:'none',
     };
   
     res.status(statusCode).cookie("token", token, options).json({
