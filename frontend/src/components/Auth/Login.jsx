3e418ec27238066082e19b4a3d252cbf3abae2f8
Abhishek kumar <88671726+tyrantstark@users.noreply.github.com>
2024-05-09 20:26:25 +0530

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://mern-job-portal-rho.vercel.app/api/v1/user/login",
         { email, password, role },
         {
           headers: {
