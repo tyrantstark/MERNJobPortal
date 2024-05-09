48511d40f33babdca90dbb12049785c031059410
Abhishek kumar <88671726+tyrantstark@users.noreply.github.com>
2024-05-09 21:15:05 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ dotenv.config({path:"./config/config.env"})
 //app.use(cors());
 app.use((req, res, next) => {
     
-    res.header("Access-Control-Allow-Origin",'https://mern-job-portal-y2jc.vercel.app' );
+    res.header("Access-Control-Allow-Origin",'http://locahost:5173' );
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true"); 
