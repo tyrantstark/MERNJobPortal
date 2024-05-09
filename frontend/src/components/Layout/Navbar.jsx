74e2ae770a9c36f1efd82a875e3c764ab2676e18
Abhishek kumar <88671726+tyrantstark@users.noreply.github.com>
2024-05-09 20:29:32 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://mern-job-portal-rho.vercel.app/api/v1/user/logout",
         {
           withCredentials: true,
         }
